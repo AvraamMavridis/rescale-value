789c8005e39aa8ddaa8c1d91b43cec2b0065cc2b
AvraamMavridis <avraammauridis@gmail.com>
2018-09-02 15:31:35 +0200

    Chore: Initial commit

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -2,9 +2,9 @@ import normalizeValue from '../src';
 
 it('Should normalize a value correctly', () => {
   const array = [1, 2, 3, 4, 5];
-  expect(normalizeValue(5, array)).toBe(1);
-  expect(normalizeValue(3, array)).toBe(0.5);
-  expect(normalizeValue(1, array)).toBe(0);
+  expect(normalizeValue(5, array)).toBe(0.5);
+  expect(normalizeValue(3, array)).toBe(0);
+  expect(normalizeValue(1, array)).toBe(-0.5);
 });
 
 it('Should throw an Error if array is undefined', () => {
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export default function normalizeValue(value : number, array : Array < number >) : number {
+export default function meanNormalization(value : number, array : Array < number >) : number {
   if(typeof value !== "number"){
     throw Error('The 1st parameter should be a number');
   }
@@ -11,8 +11,10 @@ export default function normalizeValue(value : number, array : Array < number >)
     throw Error('The 2nd parameter should not be an empty Array');
   }
 
+  const sum = array.reduce((sum, b) => sum + b, 0);
+  const avg = sum/array.length;
   const max = Math.max(...array);
   const min = Math.min(...array);
 
-  return (value - min) / (max - min);
+  return (value - avg) / (max - min);
 }
